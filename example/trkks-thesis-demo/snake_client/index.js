ad8a652af256247833931240050ce3c399c825d0
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2024-02-12 18:22:51 +0200

    TD: (not tested) Strip down the js-client for server side rendering

--- example/trkks-thesis-demo/snake_client/index.js
+++ example/trkks-thesis-demo/snake_client/index.js
@@ -1,91 +1,26 @@
-// Constants for game area.
-const CELLBOUNDS = [20, 10];
-const CELLSIZE = [64, 64];
-const BOUNDS = [CELLBOUNDS[0] * CELLSIZE[0], CELLBOUNDS[1] * CELLSIZE[1]];
-// Constants for game timing.
+/* Time in ms to give the user to get ready at startup */
 const WAIT_READY = 0;//3000;
+/* Time in ms between fetching game frames */
 const GAME_TICK = 750;
 
-// Path where the .wasm containing game logic can be queried from.
+/* Paths where the .wasm containing game logic can be queried from */
 const SNAKE_GAME_API = {
-    init:  { path: "modules/snake/new",                    method: "POST" },
-    next:  { path: "modules/snake/next_frame_wasm32_wasi", action: "stream" },
-    input: { path: "modules/snake/set_input",              method: "POST" },
+    init:  {                   path: "modules/snake/new",                    method: "POST" },
+    next:  { action: "stream", path: "modules/snake/next_frame_wasm32_wasi"                 },
+    input: {                   path: "modules/snake/set_input",              method: "POST" },
 };
 
-const CAMERA_API = {
-    get: { path: "modules/camera/scaled", method: "GET" },
-}
-
+/* Paths for migrating modules currently placed at origin */
 const MIGRATION_API = {
-    camera: { path: "camera", action: "migrate" },
+    camera: { action: "migrate", path: "camera" },
 }
-
-// Flag for running the game in debug mode.
-let debugging = true;
-
-// Flag for pausing the game.
-let paused = false;
-
-// Flag for game over.
-let gameIsOver = true;
-
-// The game running
-let gameLoopInterval = null;
-
-// Current serialized game state.
-let stateBuffer;
-
-// The visual for apple pickup.
-let applePattern;
-
-async function updateView(ctx) {
-    if (stateBuffer) {
-        // TODO: Change hardcode to wasm.exports.new(w, h);
-        const { W, H } = { W: 20, H: 10 };
-        const mem = new Uint8Array(
-            stateBuffer, 0, W * H + 1
-        );
-        const state = mem.slice(0, W * H + 1);
-        // Show the state onscreen. 
-        fillGrid(ctx, -1);
-        for (let y = 0; y < H; y++) {
-            for (let x = 0; x < W; x++) {
-                const thing = state[y * W + x];
-                drawAtGrid(ctx, x, y, thing);
-            }
-        }
-        // Set a new image as the apple pattern if the apple-spawn-flag
-        // is set.
-        if (state.at(-1) !== 0) {
-            applePattern = await getApplePattern(ctx, ...CELLSIZE);
-        }
-    }
-
-    if (paused) {
-        ctx.fillStyle = "white";
-        ctx.font      = "20px mono";
-        ctx.textAlign = "left";
-        ctx.fillText("PAUSED", 20, 20);
-    }
-
-    if (gameIsOver) {
-        // Show that the game has ended.
-        console.log("Game over.");
-        ctx.fillStyle = "red";
-        ctx.font      = "20px mono";
-        ctx.textAlign = "left";
-        ctx.fillText(`GAME OVER (Press R to restart)`, 20, 20);
-    }
-}
-
 
 /*
  * Helper that queries for results from supervisor.
  *
  * Include your arguments to (WebAssembly) function execution after the
  * apiCommand parameter.
- **/
+ */
 async function executeSupervisor(apiCommand) {
     const args = Array.prototype.slice.call(arguments, 1);
     const argStrings = args.map((x, i) => `param${i}=${x}`);
@@ -132,101 +67,15 @@ async function executeSupervisor(apiCommand) {
         }
     }
 }
- 
-function initCanvas(canvas) {
-    const ctx = canvas.getContext("2d");
-    canvas.width = BOUNDS[0];
-    canvas.height = BOUNDS[1];
 
-    return ctx;
-}
+/* Flag that shows if game is paused */
+let paused = false;
 
-/*
- * Return a pattern for the game's apple object scaled as requested.
- */
-async function getApplePattern(ctx, scaleWidth, scaleHeight) {
-    const [_, files] = await executeSupervisor(CAMERA_API.get, 6, 9);
-    const imageResp = await fetch(files[0]);
-    const imageBlob = await imageResp.blob();
-    const imageToScale = await createImageBitmap(imageBlob);
-    // Scale the image.
-    const scalingCanvas = document.createElement("canvas");
-    // Set the canvas to be the same size as the eventual image in order to
-    // have more straightforward operations below.
-    scalingCanvas.width = scaleWidth;
-    scalingCanvas.height = scaleHeight;
-    const scalingCtx = scalingCanvas.getContext("2d");
-    scalingCtx.drawImage(imageToScale, 0, 0, scaleWidth, scaleHeight);
-    // Create a pattern from the scaled image on the canvas.
-    const pattern = ctx.createPattern(scalingCanvas, "repeat");
+/* Flag that shows if game is over */
+let gameIsOver = true;
 
-    return pattern;
-}
-
-async function init(canvas) {
-    const ctx = initCanvas(canvas);
-
-    // Show starting screen.
-    gameOver();
-    updateView(ctx);
-
-    // Set initial apple pattern.
-    applePattern = await getApplePattern(ctx, ...CELLSIZE);
-
-    return ctx;
-}
-
-/*
-* Set current fillstyle based on thing.
-*/
-function setFillStyleOnTing(ctx, thing) {
-    switch (thing) {
-        case 0:
-            ctx.fillStyle = applePattern;
-            break;
-        case 1:
-            ctx.fillStyle = "blue";
-            break;
-        case 2:
-            ctx.fillStyle = "black";
-            break;
-        case 3:
-            ctx.fillStyle = "lightBlue";
-            break;
-        case 4:
-            ctx.fillStyle = "red";
-            break;
-        default:
-            ctx.fillStyle = "pink";
-            break;
-    }
-}
-
-/*
-* Draw the thing at the grid coordinate.
-*/
-function drawAtGrid(ctx, x, y, thing) {
-    setFillStyleOnTing(ctx, thing);
-
-    ctx.fillRect(
-        x * CELLSIZE[0],
-        y * CELLSIZE[1],
-        CELLSIZE[0],
-        CELLSIZE[1]
-    );
-}
-
-/*
-* Clear the canvas.
-*/
-function fillGrid(ctx, thing) {
-    setFillStyleOnTing(ctx, thing);
-    for (let y = 0; y < CELLBOUNDS[1]; y++) {
-        for (let x = 0; x < CELLBOUNDS[0]; x++) {
-            drawAtGrid(ctx, x, y);
-        }
-    }
-}
+/* Timer that updates the game */
+let gameLoopInterval = null;
 
 function gameOver() {
     // Stop the running game.
@@ -235,84 +84,83 @@ function gameOver() {
     gameIsOver = true;
 }
 
-/*
-* Add __global__ keyboard controls.
-*/
-function initKeyDownControl(ctx) {
-    document.addEventListener("keydown",
-        async function(e) {
-            let code = null;
-            // Control the running game.
-            switch (e.key) {
-                case "ArrowUp"   : code = 0; break;
-                case "ArrowDown" : code = 1; break;
-                case "ArrowLeft" : code = 2; break;
-                case "ArrowRight": code = 3; break;
-            }
-            if (code !== null) {
-                // Game observes this input.
-                executeSupervisor(SNAKE_GAME_API.input, code);
-                return;
-            }
+async function updateView(ctx, imgBlob) {
+    if (imgBlob) {
+        const bitmap = await createImageBitmap(imgBlob);
+        ctx.drawImage(bitmap, 0, 0, 200, 200);
+    }
 
-            // Control the application.
-            switch (e.key) {
-                case "d":
-                    debugging = !debugging;
-                    break;
-                case "j":
-                    // Allow manually "ticking" the game forward.
-                    await gameUpdate(ctx);
-                    break;
-                case "m":
-                    // Allow changing the camera source.
-                    executeSupervisor(MIGRATION_API.camera);
-                    break;
-                case "p":
-                    paused = !paused;
-                    break;
-                case "r":
-                    await restartGame(ctx, false);
-                    break;
-            }
+    if (paused) {
+        ctx.fillStyle = "white";
+        ctx.font      = "20px mono";
+        ctx.textAlign = "left";
+        ctx.fillText("PAUSED", 20, 20);
+    }
 
-        }
-    );
+    if (gameIsOver) {
+        // Show that the game has ended.
+        console.log("Game over.");
+        ctx.fillStyle = "red";
+        ctx.font      = "20px mono";
+        ctx.textAlign = "left";
+        ctx.fillText(`GAME OVER (Press R to restart)`, 20, 20);
+    }
 }
 
+
 /*
- * Update the game one step forward.
- **/
-async function gameUpdate(ctx) {
+ * Get blob of the next image frame of the game.
+ */
+async function updateGame(ctx) {
     console.log("-- tick --");
-    const response = await fetch(SNAKE_GAME_API.next.action + "/" + SNAKE_GAME_API.next.path);
+    
+    const response = await fetch(
+        SNAKE_GAME_API.next.action + "/" + SNAKE_GAME_API.next.path
+    );
+
     let chunks = [];
+    console.log("Collecting stream...");
     for await (const chunk of response.body) {
-        console.log("STREAM:", chunk);
+        console.log("Chunk:", chunk);
         chunks.push(chunk);
     }
-    console.log("Game state stream has ended with:", chunks);
-    let buffer = new Uint8Array(chunks.reduce((acc, x) => acc + x.length, 0));
-    let start = 0;
-    for (const chunk of chunks) {
-        buffer.set(chunk, start);
-        start += chunk.length;
-    }
-    console.log("Buffer built is:", buffer);
+    console.log("Stream has ended with:", chunks);
 
-    const gameOverCode = buffer[0];
-    stateBuffer = buffer.slice(1);
-
-    if (gameOverCode !== 0) {
-        gameOver();
+    // Throw if game is over.
+    if (chunks[0][0] !== 0) {
+        throw `game over code: ${chunks[0][0]}`;
     }
+
+    // Pick out the part where game over code is stored.
+    chunks[0] = chunks[0].slice(1);
+
+    // Return the part that contains the JPEG image.
+    return new Blob(chunks);
+}
+
+async function init(canvas) {
+    const ctx = canvas.getContext("2d");
+    canvas.width = 256;
+    canvas.height = 256;
+
+    // Show starting screen.
+    gameOver();
+    updateView(ctx);
+
+    return ctx;
 }
 
 async function gameLoop(ctx) {
+    let imgBlob;
     if (!paused) {
-        await gameUpdate(ctx);
+        try {
+            imgBlob = await updateGame(ctx);
+        } catch (e) {
+            console.log(e);
+            gameOver();
+        }
     }
-    await updateView(ctx);
+    await updateView(ctx, imgBlob);
 }
 
 async function restartGame(ctx, dowait=true) {
@@ -339,13 +187,50 @@ async function restartGame(ctx, dowait=true) {
     }
 }
 
-// Initialize first game.
+/*
+ * Add keyboard controls.
+ */
+function initKeyDownControl(ctx) {
+    document.addEventListener("keydown",
+        async function(e) {
+            let code = null;
+            // Control the running game.
+            switch (e.key) {
+                case "ArrowUp"   : code = 0; break;
+                case "ArrowDown" : code = 1; break;
+                case "ArrowLeft" : code = 2; break;
+                case "ArrowRight": code = 3; break;
+            }
+            if (code !== null) {
+                // Game observes this input.
+                executeSupervisor(SNAKE_GAME_API.input, code);
+                return;
+            }
+
+            // Control the application.
+            switch (e.key) {
+                case "j":
+                    // Allow manually "ticking" the game forward.
+                    await updateGame(ctx);
+                    break;
+                case "m":
+                    // Allow changing the camera source.
+                    executeSupervisor(MIGRATION_API.camera);
+                    break;
+                case "p":
+                    paused = !paused;
+                    break;
+                case "r":
+                    await restartGame(ctx, false);
+                    break;
+            }
+
+        }
+    );
+}
+
 window.onload = async () => {
     const ctx = await init(document.getElementById("canvas"));
-
     initKeyDownControl(ctx);
-
-    // User has to start the game by hitting 'r'.
-    //await restartGame(ctx);
 };
 
